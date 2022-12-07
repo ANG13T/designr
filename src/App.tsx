b8c912da38c996444331e9c9a87388598bd54c23
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 15:16:56 -0800

    feat: init designr logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/designr-logo.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wooo designr
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
