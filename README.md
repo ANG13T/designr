57b432d557490f08a8734332bcf8a0925a9748f7
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 10:47:05 -0800

    feat: update styling

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - create a palette (palette name and/ url)
 - share a palette
 - select colors, fonts, etc
+- dark + white (text) and rainbow touch theme (dots) and input rainbow
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Wooo designr
         </p>
         <a
           className="App-link"
