3bb518da5e72324a5078a28c1728c600db7223ed
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-08 08:32:29 -0800

    feat: init validation for login and register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { goTo } from 'react-chrome-extension-router';
 import logo from '../../assets/designr-logo.png';
 import Register from '../Register/Register';
 import './Login.css';
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('border-gray-300'); 
+  const [passwordErrror, setPasswordError] = useState('border-gray-300');
+  const [loading, setLoading] = useState(false);
+
+  const validateEmail = (email: string) => {
+    return email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const checkError = () => {
+    if(!loading) {
+      let isValid = true;
+      if(email.length > 0 && validateEmail(email)) {
+        setEmailError('border-gray-300')
+      } else {
+        setEmailError('border-red-400')
+        isValid = false;
+      }
+
+      if(password.length < 6) {
+        setPasswordError('border-red-400')
+        isValid = false;
+      } else {
+        setPasswordError('border-gray-300')
+      }
+
+      if(isValid) {
+        loginUser();
+      }
+    }
+  }
+
+  const loginUser = () => {
+    setLoading(true);
+  }
+
+  const googleLogin = () => {
+
+  }
+
     return (
         <div className="App">
             <header className="App-header border-gradient border-gradient-purple">
@@ -17,7 +63,8 @@ function Login() {
                                 type="email"
                                 placeholder='Enter email address'
                                 id="email"
-                                className="text-sm text-black px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                onChange={(ev) => setEmail(ev.target.value)}
+                                className={`text-sm text-black px-4 py-2 transition duration-300 border ${emailError} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
                             />
                         </div>
                         <div className="flex flex-col space-y-1">
@@ -28,16 +75,21 @@ function Login() {
                                 type="password"
                                 id="password"
                                 placeholder='Enter password'
-                                className="text-sm text-black px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                onChange={(ev) => setPassword(ev.target.value)}
+                                className={`text-sm text-black px-4 py-2 transition duration-300 border ${passwordErrror} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
                             />
                         </div>
 
                         <div>
                             <button
-                                type="submit"
+                                type="button"
+                                onClick={() => checkError()}
                                 className="text-sm w-full px-4 py-2 font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4 mb-1"
                             >
-                                Log in
+                                {!loading ? 'Login' : <svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+    </svg>}
                             </button>
                         </div>
                         <div className="flex flex-col space-y-3">
@@ -48,7 +100,7 @@ function Login() {
                             </span>
                             <div className="flex flex-col mt-1">
                             <div className='w-full mr-10'>
-                                <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mb-2">
+                                <button type="button" onClick={() => googleLogin()} className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mb-2">
                                     <svg className="mr-2 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
                                     Sign in with Google
                                 </button>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -22,20 +22,22 @@ function Register() {
 
   const checkError = () => {
     if(!loading) {
-      if(validateEmail(email)) {
+      let isValid = true;
+      if(email.length > 0 && validateEmail(email)) {
         setEmailError('border-gray-300')
       } else {
         setEmailError('border-red-400')
+        isValid = false;
       }
 
       if(password.length < 6) {
         setPasswordError('border-red-400')
+        isValid = false;
       } else {
         setPasswordError('border-gray-300')
       }
 
-      // Create User if no error
-      if(passwordErrror == 'border-gray-300' && emailError == 'border-gray-300') {
+      if(isValid) {
         createUser();
       }
     }
@@ -74,7 +76,7 @@ function Register() {
                                 id="password"
                                 placeholder='Enter password'
                                 onChange={(ev) => setPassword(ev.target.value)}
-                                className="text-sm text-black px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                className={`text-sm text-black px-4 py-2 transition duration-300 border ${passwordErrror} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
                             />
                         </div>
 
@@ -97,7 +99,7 @@ function Register() {
                             </span>
                             <div className="flex flex-col mt-1">
                             <div className='w-full mr-10'>
-                                <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mb-2">
+                                <button type="button" onClick={() => googleSignUp()} className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mb-2">
                                     <svg className="mr-2 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
                                     Sign up with Google
                                 </button>
