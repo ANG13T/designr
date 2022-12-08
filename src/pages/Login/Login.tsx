42088eaf0259b9a6ed9cbbd31b9062ff6a547e0f
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 18:58:00 -0800

    feat: init Login and Register

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/designr-logo.png';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <h3>Login</h3>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/designr-logo.png';
+import './Register.css';
+
+function Register() {
+  return (
+    <div className="Register">
+      <h3>Register</h3>
+    </div>
+  );
+}
+
+export default Register;
