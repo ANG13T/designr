9ed5e1c9e71809c893d9725148a118e3589c5b45
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 18:42:37 -0800

    feat: init react-chrome-extension-router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { MemoryRouter as Router } from 'react-router-dom';
+import {
+  Router
+} from 'react-chrome-extension-router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>
 );
