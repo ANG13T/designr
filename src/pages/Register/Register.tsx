4020983b8f72d437cb1c8205e506ed8a3cb02451
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-08 08:23:13 -0800

    feat: init `checkError`

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { goTo } from 'react-chrome-extension-router';
 import logo from '../../assets/designr-logo.png';
 import Login from '../Login/Login';
 import './Register.css';
 
 function Register() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('border-gray-300'); 
+  const [passwordErrror, setPasswordError] = useState('border-gray-300');
+  const [loading, setLoading] = useState(false);
+
+  const validateEmail = (email: string) => {
+    return email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const checkError = () => {
+    if(!loading) {
+      if(validateEmail(email)) {
+        setEmailError('border-gray-300')
+      } else {
+        setEmailError('border-red-400')
+      }
+
+      if(password.length < 6) {
+        setPasswordError('border-red-400')
+      } else {
+        setPasswordError('border-gray-300')
+      }
+
+      // Create User if no error
+      if(passwordErrror == 'border-gray-300' && emailError == 'border-gray-300') {
+        createUser();
+      }
+    }
+  }
+
+  const createUser = () => {
+    setLoading(true);
+  }
+
+  const googleSignUp = () => {
+
+  }
+
   return (
     <div className="App">
             <header className="App-header border-gradient border-gradient-purple">
@@ -17,7 +61,8 @@ function Register() {
                                 type="email"
                                 placeholder='Enter email address'
                                 id="email"
-                                className="text-sm text-black px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                                onChange={(ev) => setEmail(ev.target.value)}
+                                className={`text-sm text-black px-4 py-2 transition duration-300 border ${emailError} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
                             />
                         </div>
                         <div className="flex flex-col space-y-1">
@@ -28,16 +73,20 @@ function Register() {
                                 type="password"
                                 id="password"
                                 placeholder='Enter password'
+                                onChange={(ev) => setPassword(ev.target.value)}
                                 className="text-sm text-black px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
                             />
                         </div>
 
                         <div>
                             <button
-                                type="submit"
+                                onClick={() => checkError()}
                                 className="text-sm w-full px-4 py-2 font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4 mb-1"
                             >
-                                Create Account
+                                {!loading ? 'Create Account' : <svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+    </svg>}
                             </button>
                         </div>
                         <div className="flex flex-col space-y-3">
