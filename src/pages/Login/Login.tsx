105cddcbe5ffb2ab7f7468857e18f8d71f62a642
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 19:52:48 -0800

    feat: finish styling `Login`

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -33,7 +33,7 @@ function Login() {
                         <div>
                             <button
                                 type="submit"
-                                className="text-sm w-full px-4 py-2 font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                                className="text-sm w-full px-4 py-2 font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4 mb-1"
                             >
                                 Log in
                             </button>
@@ -44,7 +44,7 @@ function Login() {
                                 <span className="font-normal text-white text-sm">or login with</span>
                                 <span className="h-px bg-gray-400 w-14"></span>
                             </span>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col mt-1">
                             <div className='w-full mr-10'>
                                 <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mb-2">
                                     <svg className="mr-2 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
@@ -55,7 +55,7 @@ function Login() {
                         </div>
                         <p className="text-sm font-light text-white">
                                 Don't have an account? <a href="/signup"
-                                    className="font-medium text-blue-300">Create an
+                                    className="font-medium text-blue-400">Create an
                                     Account</a>
                             </p>
                     </form>
