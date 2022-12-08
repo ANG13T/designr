045e3dd9f3ea1bcb98f5fb0adfa081d112c4c092
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-08 13:53:14 -0800

    feat: update Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import { supabase } from './services/supabaseClient';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   const [session, setSession] = useState<any>(null)
@@ -40,23 +41,7 @@ function App() {
       </div>
 
       </header>
-    </div>: <div className="App">
-        <header className="App-header border-gradient border-gradient-purple">
-          <img src={logo} className="App-logo mb-20" alt="logo" />
-
-          <button onClick={() => goTo(Login)} type="button" className="c-button text-white bg-transparent hover:bg-white hover:text-black border border-white focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-10 mr-10 mb-10 mt-5">
-            Authed inside
-          </button>
-
-
-        <div className='w-full ml-10 mr-10'>
-          <button onClick={() => goTo(Register)} type="button" className="c-button text-white bg-transparent hover:bg-white hover:text-black border border-white focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2">
-            Create an Account
-          </button>
-        </div>
-
-        </header>
-      </div>}
+    </div>: <Dashboard></Dashboard>}
       
     </div>
   );
