0cfde7f5f0004c2e472db65869cbc5280fe85005
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-29 15:24:31 +0900

    feat: imporve home styles

--- designr/pages/index.tsx
+++ designr/pages/index.tsx
@@ -1,8 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import logo from '/designr-logo.png';
 
 export default function Home() {
   return (
