a41487158430463698f86d5ca2fc8c3f6722c0fb
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-29 20:52:25 +0900

    feat: init supabase

--- designr/pages/_app.tsx
+++ designr/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
