3743628456c23edce8c5d5744cc692891585abb2
angelina-tsuboi <angelina.t1832@gmail.com>
2023-02-14 13:18:29 -0800

    feat: save view start page complete

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+let startButton = document.getElementById("startButton");
+let homePage = document.getElementById("home");
+let mainPage = document.getElementById("main");
+
 function setViewHome() {
-    chrome.storage.sync.set({ "viewHome": "true" });
+    chrome.storage.sync.set({ "viewHome": "true" }, () => console.log("done"));
 }
 
 async function getViewHome() {
-    chrome.storage.sync.get(["viewHome"], function(items) {
-        return items;
+    await chrome.storage.sync.get(["viewHome"], function (result) {
+        console.log(result);
+        if (result && result["viewHome"] == "true") {
+            mainPage.hidden = false;
+            homePage.hidden = true;
+        }
     });
 }
 
-function initiate() {
-    console.log(getViewHome())
-}
 
-initiate();
+getViewHome();
+mainPage.hidden = true;
+homePage.hidden = false;
+
+startButton.addEventListener('click', function () {
+    setViewHome();
+    mainPage.hidden = false;
+    homePage.hidden = true;
+});
