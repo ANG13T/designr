6c0d94c808daf19d321eaa5d35c42da7da9454b7
angelina-tsuboi <angelina.t1832@gmail.com>
2023-02-15 22:10:51 -0800

    feat: show new palettes as rows

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let startButton = document.getElementById("startButton");
 let homePage = document.getElementById("home");
 let mainPage = document.getElementById("main");
+let paletteTableContent = document.getElementById('paletteTable').getElementsByTagName('tbody')[0];
 
 function setViewHome() {
     chrome.storage.sync.set({ "viewHome": "true" }, () => console.log("done"));
@@ -15,22 +16,47 @@ async function getViewHome() {
     });
 }
 
-async function retreievePalettes() {
-    chrome.storage.local.get('palettes', function (result) {
-        return result.palettes;
+async function retreivePalettes() {
+    chrome.storage.local.get({ palettes: [] }, function (result) {
+        let resultPal = result.palettes;
+        console.log("palettes", resultPal);
+
+        if (resultPal.length > 0) {
+            resultPal.forEach((pa) => {
+                let newRow = paletteTableContent.insertRow(1);
+                newRow.innerHTML = `
+                    <tr>
+                        <th>
+                            <label class="customcheckbox">
+                                <input type="checkbox" class="listCheckbox">
+                                <span class="checkmark"></span>
+                            </label>
+                        </th>
+                        <td>${pa.name}</td>
+                        <td>${pa.createdAt}</td>
+                        <td>${pa.elements}</td>
+                    </tr>
+                `;
+            })
+        }
     });
 }
 
 function setPalettes(palettes) {
-    chrome.storage.local.set({palettes: palettes}, function () {
+    chrome.storage.local.set({ palettes: palettes }, function () {
 
     })
 }
 
 
 getViewHome();
+updatePaletteUI();
 mainPage.hidden = true;
 homePage.hidden = false;
+
+async function updatePaletteUI() {
+    await retreivePalettes();
+}
 
 startButton.addEventListener('click', function () {
     setViewHome();
