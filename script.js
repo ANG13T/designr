65af79b7f400b4ba974a0ced455c0c623de72050
angelina-tsuboi <angelina.t1832@gmail.com>
2023-02-16 19:09:28 -0800

    feat: init basic createPalette

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let homePage = document.getElementById("home");
 let mainPage = document.getElementById("main");
 let paletteTableNone = document.getElementById("none-palette");
 let paletteTable = document.getElementById("paletteTable");
+let paletteNameInput = document.getElementById("paletteNameInput");
 let viewSettingsButton = document.getElementById("view-settings");
 let paletteTableContent = document.getElementById('paletteTable').getElementsByTagName('tbody')[0];
 
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
-const openModalBtn = document.getElementById("create-palette");
+const createPaletteBtn = document.getElementById("create-palette");
 const closeModalBtn = document.getElementById("btn-close");
+
+let palettes = [];
 
 function setViewHome() {
     chrome.storage.sync.set({ "viewHome": "true" }, () => console.log("done"));
@@ -27,25 +30,11 @@ async function getViewHome() {
 async function retreivePalettes() {
     chrome.storage.local.get({ palettes: [] }, function (result) {
         let resultPal = result.palettes;
+        palettes = resultPal;
         console.log("palettes", resultPal);
 
         if (resultPal.length > 0) {
-            resultPal.forEach((pa) => {
-                let newRow = paletteTableContent.insertRow(1);
-                newRow.innerHTML = `
-                    <tr>
-                        <th>
-                            <label class="customcheckbox">
-                                <input type="checkbox" class="listCheckbox">
-                                <span class="checkmark"></span>
-                            </label>
-                        </th>
-                        <td>${pa.name}</td>
-                        <td>${pa.createdAt}</td>
-                        <td>${pa.elements}</td>
-                    </tr>
-                `;
-            })
+            renderPalettes(resultPal);
         } else {
             paletteTable.hidden = true;
             paletteTableNone.hidden = false;
@@ -53,8 +42,28 @@ async function retreivePalettes() {
     });
 }
 
-function setPalettes(palettes) {
-    chrome.storage.local.set({ palettes: palettes }, function () {
+function renderPalettes(selectedPal) {
+    paletteTableContent.innerHTML = "";
+    selectedPal.forEach((pa) => {
+        let newRow = paletteTableContent.insertRow(1);
+        newRow.innerHTML = `
+            <tr>
+                <th>
+                    <label class="customcheckbox">
+                        <input type="checkbox" class="listCheckbox">
+                        <span class="checkmark"></span>
+                    </label>
+                </th>
+                <td>${pa.name}</td>
+                <td>${pa.createdAt}</td>
+                <td>${pa.elements}</td>
+            </tr>
+        `;
+    })
+}
+
+function setPalettes(selectedPal) {
+    chrome.storage.local.set({ palettes: selectedPal }, function () {
 
     })
 }
@@ -63,7 +72,25 @@ const closeModal = function () {
     console.log(modal)
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-  };
+};
+
+const createPalette = function(paletteName) {
+    paletteNameInput.classList.remove("error");
+    if(paletteName.length > 0 && paletteName.length < 20) {
+        let date = new Date();
+        let formattedDate = `${date.getMonth} / ${date.getDate()} / ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+        let newPalette = {
+            name: paletteName,
+            elements: 0,
+            createdDate: formattedDate
+        }
+        palettes.push(newPalette);
+        setPalettes(palettes);
+        renderPalettes(palettes);
+    } else {
+        paletteNameInput.classList.add("error");
+    }
+}
 
 
 getViewHome();
@@ -84,27 +111,25 @@ startButton.addEventListener('click', function () {
 });
 
 
-// Modal Stuff
-// close modal function
 closeModal()
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-  
-  // close the modal when the close button and overlay is clicked
-  closeModalBtn.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
-  
-  // close modal when the Esc key is pressed
-  document.addEventListener("keydown", function (e) {
+
+document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-      closeModal();
+        closeModal();
     }
-  });
-  
-  // open modal function
-  const openModal = function () {
+});
+
+
+const openModal = function () {
     console.log("open modal")
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-  };
-  // open modal event
-  openModalBtn.addEventListener("click", openModal);
+};
+
+createPaletteBtn.addEventListener("click", () => {
+    let palName = paletteNameInput.value;
+    createPalette(palName);
+});
