4082dd450deb47024cc4c90535d231c38f42489b
angelina-tsuboi <angelina.t1832@gmail.com>
2023-02-25 14:08:50 -0800

    feat: init rudimentary designrClick code

--- js/designr.js
+++ js/designr.js
@@ -539,6 +539,12 @@ function designrClick(e)
 	e.stopPropagation();
 	var element = document.defaultView.getComputedStyle(this, null);
 	console.log("click something", element)
+	chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}); 
+	})
 }
 
 function designrMouseOver(e)
